14398f2421d273c39d3e7d42b56674f614c45b01
Gpolly <gngugi17@gmail.com>
2019-11-29 17:00:32 +0300

    I have updated my js file to pick the elements keyin by the user

--- js/form.js
+++ js/form.js
@@ -1,14 +1,28 @@
 function mystory() {
-//   picking the values which are inside by the user
-var cc = document.forms["story"]["year"]
-//var yy = document.forms["story"]["year"]
-//var mm = document.forms["story"]["month"]
-//var dd = document.forms["story"]["day"]
-//var gender= document.forms["story"]["year"]
-alert(cc);
-}
-/*
-function day (cc,yy,mm,dd){
-    return ((((cc/4)-2*cc-1)+((5*yy/4)) + ((26*(mm+1)/10)) + dd)%7)+1
-    }
-*/
+    function day (cc,yy,mm,dd){
+        return ((((cc/4)-2*cc-1)+((5*yy/4)) + ((26*(mm+1)/10)) + dd)%7)+1
+  }
+    //picking the item inputted by the user
+  var cc1 = document.forms["story"]["year"].value;
+    //this is to pick the left two(cc)   
+  var cc = cc1.slice(0,2)
+  var yy1 = document.forms["story"]["year"].value;
+  // this is to select right two(yy)
+  var yy = yy1.slice(2,4)
+  var mm = document.forms["story"]["month"].value;
+  var dd = document.forms["story"]["day"].value;
+  var e = document.getElementById("ddlViewBy");
+  var gender = e.options[e.selectedIndex].text; 
+  
+//   alert(cc);
+//   alert(yy);
+//   alert(mm);
+//   alert(dd);
+//   alert(gender);
+
+  var result=day(cc,yy,mm,dd).toFixed();
+  alert(result);  
+  
+  }
+  
+

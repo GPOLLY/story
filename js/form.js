d450df4bb31972eac711fd4e2bcc741f6f3db01c
Gpolly <gngugi17@gmail.com>
2019-11-29 23:13:31 +0300

    i have correct the indentation in js file

--- js/form.js
+++ js/form.js
@@ -1,6 +1,6 @@
 function mystory() {
     function day (cc,yy,mm,dd){
-        return ((((cc/4)-2*cc-1)+((5*yy/4)) + ((26*(mm+1)/10)) + dd)%7)+1
+      return ((((cc/4)-2*cc-1)+((5*yy/4)) + ((26*(mm+1)/10)) + dd)%7)+1
   }
     
   var cc1 = document.forms["story"]["year"].value;

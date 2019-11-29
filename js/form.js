556e31855b8f2edb78b080740f6ae890ca44ee5b
Gpolly <gngugi17@gmail.com>
2019-11-29 15:28:35 +0300

    i have link the java script inside the index html page

---
+++ js/form.js
@@ -0,0 +1,14 @@
+function mystory() {
+//   picking the values which are inside by the user
+var cc = document.forms["story"]["year"]
+//var yy = document.forms["story"]["year"]
+//var mm = document.forms["story"]["month"]
+//var dd = document.forms["story"]["day"]
+//var gender= document.forms["story"]["year"]
+alert(cc);
+}
+/*
+function day (cc,yy,mm,dd){
+    return ((((cc/4)-2*cc-1)+((5*yy/4)) + ((26*(mm+1)/10)) + dd)%7)+1
+    }
+*/
